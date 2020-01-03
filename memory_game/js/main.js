b2dbbeb7b9742d90d2c437e811d356eb6d8bdabb
intraG <50717686+Intravenous@users.noreply.github.com>
2020-01-03 17:32:11 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
 
+let cardsInPlay = []; 
+
+let cardOne = cards[0];
+let cardTwo = cards[2];
+let cardFour; // added by myself to remove console error
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry try again");
+}
+	
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour)
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardFour);
